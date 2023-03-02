e0fd4b55778253457432bae55dcd293cfaf3264f
NeelakandanV <hari0709venkatesan@gmail.com>
2023-03-02 15:26:05 +0530

    first commit

---
+++ oops.js
@@ -0,0 +1,48 @@
+// 3. Write a “person” class to hold all the details.
+
+//class person{
+//    constructor(name,age){
+//        this.name = name;
+//        this.age = age;
+//    }
+//    details = () => {
+//        var det ={
+//            Education : 'BE',
+//            Marital_status : 'single',
+//            DOB : '07/09/2000',
+//            Job_title : 'software developer',
+//            Mob_no : 18454156214,
+//            Email_id : 'abc@gmail.com'
+//        }
+//        return `hi ${this.name}` ;
+//
+//    }
+//    
+//}
+//var data =new person('Hari',23);
+//console.log(data.details())
+
+// output
+// hi Hari
+
+
+
+
+// 4.write a class to calculate the uber price.
+
+//class uber{
+//    constructor(dist,farePerKm,waitTime){
+//        this.dist=dist;
+//        this.farePerKm=farePerKm;
+//        this.waitTime=waitTime;
+//
+//    }
+//    calc =() =>{
+//        return (`${this.dist}`*`${this.farePerKm}`)+(`${this.waitTime}`*50)
+//    }
+//}
+//var price = new uber(25,8,2.5);
+//console.log(price.calc());
+
+// output
+// 325
